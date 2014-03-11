f695ea382de14546c97b24963b9670aca7170aab
ndr <ndr@qef.io>
2014-03-11 19:05:07 +0100

    Define filter-lists for behaviors, objects, tags

--- src/lt/plugins/aleph.cljs
+++ src/lt/plugins/aleph.cljs
@@ -27,6 +27,14 @@
   [t]
   (map deref (object/by-tag t)))
 
+(defn t-enlist [t]
+ (map #(hash-map :tag (str (key %))
+                 :behaviors (val %)) @t))
+
+(def t-list (scmd/filter-list {:items (t-enlist object/tags)
+                               :key :tag
+                               :placeholder "Tag"}))
+
 
 ;;;; object to behavior/tag ;;;;
 
@@ -39,6 +47,15 @@
   "Given an object instance, return the associated behaviors."
   [o]
   (mapcat t->b (o->t o)))
+
+(defn o-enlist [o]
+  (map #(hash-map :type (-> % val deref :lt.object/type str)
+                  :tags (-> % val deref :tags)
+                  :listeners (-> % val deref :listeners)) @o))
+
+(def o-list (scmd/filter-list {:items (o-enlist object/instances)
+                               :key :type
+                               :placeholder "Object"}))
 
 
 ;;;; behavior to object/tag ;;;;
@@ -60,3 +77,13 @@
 
 (defn b->o [b]
   (mapcat #(object/by-tag %) (keys (b->t b))))
+
+;;; behavior filter-list
+
+(defn b-enlist [b]
+  (map #(hash-map :name (str (:name %))
+                  :triggers (:triggers %)) (vals @b)))
+
+(def b-list (scmd/filter-list {:items (b-enlist object/behaviors)
+                               :key :name
+                               :placeholder "Behaviors"}))
