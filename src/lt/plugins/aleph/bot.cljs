589a7deb568495cf305ca57f9d8a174901f805e7
ndr <ndr@qef.io>
2014-06-21 15:55:07 +0200

    Bestow docstrings upon aleph.bot

--- src/lt/plugins/aleph/bot.cljs
+++ src/lt/plugins/aleph/bot.cljs
@@ -29,6 +29,11 @@
   (some bs (map k|coll (val t))))
 
 (defn b->t
+  "Given a behavior name or sequence thereof, returns a map of tags which
+   associate any of them.
+   Keys are tag names, associated to their full list of behaviors.
+
+   The returned map is a subset of the `object/tags` index."
   [bs]
   (let [b-set (->set bs)]
     (->> @object/tags
@@ -39,6 +44,11 @@
   (some bs (flat-listeners (val o))))
 
 (defn b->o
+  "Given a behavior name or sequence thereof, returns a map of objects which
+   use any of them.
+   Keys are integer ids, associated to their object instance.
+
+   The returned map is a subset of the `object/instances` index."
   [bs]
   (let [b-set (->set bs)]
     (->> @object/instances
@@ -52,8 +62,19 @@
     (into {} (distinct (mapcat relator elements))))
 
 (defprotocol ObjectRelator
-  (o->b [o] "Return behaviors related to the object")
-  (o->t [o] "Return tags related to the object"))
+  (o->b [o]
+   "Given an object type, id, or instance, or a sequence thereof, returns a map
+    of their behaviors.
+    Keys are behavior names, associated to their full value: name, triggers, reaction.
+
+    The returned map is a subset of the `object/behaviors` index.")
+
+  (o->t [o]
+   "Given an object type, id, or instance, or a sequence thereof, returns a map
+    of their tags.
+    Keys are tag names, associated to their full list of behaviors.
+
+    The returned map is a subset of the `object/tags` index."))
 
 (extend-protocol ObjectRelator
   ;; concision?
@@ -123,16 +144,19 @@
 ;;; tag to object/behavior
 
 (defn ->b-val
-  "Given a behavior name, returns the full behavior map."
+  "Given an index of behaviors and a behavior name, returns the full behavior
+   value. Arguments passed to the behavior are stored as kv `::args [arg & more]`."
   [index b]
   (if (coll? b)
     (merge ((first b) index) {::args (into [] (rest b))})
     (b index)))
 
 (defn t->b
-  "Given a tag or sequence thereof, returns a map of the associated behaviors.
-   Aguments passed by a tag will be stored in the behavior's entry as kv
-   `:lt.plugins.aleph.bot/args [args]`."
+  "Given a tag or sequence thereof, returns a map of the behaviors associated
+   by any of them.
+   Keys are behavior names, associated to their full value: name, triggers, reaction.
+   Arguments passed by a tag will be stored in the behavior's entry as kv
+   `:lt.plugins.aleph.bot/args [arg & more]`."
   [ts]
   (let [t-set (->set ts)
         bs (object/tags->behaviors t-set)
@@ -144,7 +168,10 @@
   (some ts (:tags @(val o))))
 
 (defn t->o
-  "Given a tag or sequence thereof, returns a map of the associated objects."
+  "Given a tag or sequence thereof, returns a map of the objects tagged with any
+   of them.
+
+   The returned map is a subset of the `object/instances` index."
   [ts]
   (let [t-set (->set ts)]
     ;; there seem to be inconsistent performance issues
@@ -164,7 +191,13 @@
 (defn any-triggers? [triggers b]
   (some triggers (:triggers (val b))))
 
-(defn triggers->behaviors [triggers]
+(defn triggers->behaviors
+  "Given a trigger or a sequence thereof, returns a map of behaviors listening
+   for any of them.
+   Keys are behavior names, associated to their full value: name, triggers, reaction.
+
+   The returned map is a subset of the `object/behaviors` index."
+  [triggers]
   (let [trigger-set (->set triggers)]
     (->> @object/behaviors
          (filter #(any-triggers? trigger-set %))
@@ -178,5 +211,15 @@
    :o {:b o->b :t o->t}
    :t {:b t->b :o t->o}})
 
-(defn relate [elems tail head]
+(defn relate
+  "Identifies associations between BOT elements.
+
+   Takes one element or a sequence thereof, reads them as elements of type `tail`,
+   and returns the associated elements of type `head`.
+
+   Tail and head are keywords, and can be:
+   - `:b` for behaviors,
+   - `:o` for objects,
+   - `:t` for tags."
+  [elems tail head]
   ((-> relators tail head) elems))
