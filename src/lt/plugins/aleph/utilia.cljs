351c71b3d1f0a4f8f30e1b9605969e301e3b478a
ndr <ndr@qef.io>
2014-07-27 20:48:34 +0200

    Add utilia

---
+++ src/lt/plugins/aleph/utilia.cljs
@@ -0,0 +1,25 @@
+(ns lt.plugins.aleph.utilia
+  (:require [lt.objs.files :as fs]))
+
+
+(defn deep-merge [& xs]
+  (let [ms (map #(or % {}) xs)]
+    (apply
+     (fn f [& ys]
+       (if (every? map? ys)
+         (apply merge-with f ys)
+         (last ys)))
+     ms)))
+
+
+(defn atom? [x]
+  (satisfies? IDeref x))
+
+(defn if-deref [x]
+  (if (atom? x)
+    @x
+    x))
+
+
+(def lt-home-folder (fs/resolve (fs/lt-home) ".."))
+(def lt-home-project (fs/join lt-home-folder "project.clj"))
