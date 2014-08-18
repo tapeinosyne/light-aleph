d640dcd3e70b9f1a2253d9d92f470231184df116
ndr <ndr@qef.io>
2014-08-18 17:38:15 +0200

    Nuke unused file
    Top-level ns can be reinstated when issues with cross-ns
    behavior-loading are solved.

--- src/lt/plugins/aleph.cljs
+++
@@ -1,7 +0,0 @@
-(ns lt.plugins.aleph
-  (:require [lt.plugins.aleph.browser :as browse]
-            [lt.plugins.aleph.inspector :as inspect]
-            [lt.plugins.aleph.monitor :as monitor]
-            [lt.objs.command :as cmd]
-            [lt.objs.tabs :as tab]
-            [lt.object :as object]))
